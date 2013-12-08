67e672d32610d1cfbbc4ad16c9202e5d75bdb718
Norbert Scheller <nscheller@actian.com>
2013-12-08 15:48:09 +0100

    BinaryTreeSuite runnable within Eclipse

--- wk5_actorbintree/src/test/scala/actorbintree/BinaryTreeSuite.scala
+++ wk5_actorbintree/src/test/scala/actorbintree/BinaryTreeSuite.scala
@@ -11,7 +11,10 @@ import scala.util.Random
 import scala.concurrent.duration._
 import org.scalatest.FunSuite
 
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
 
+@RunWith(classOf[JUnitRunner])
 class BinaryTreeSuite(_system: ActorSystem) extends TestKit(_system) with FunSuite with ShouldMatchers with BeforeAndAfterAll with ImplicitSender 
 {
 
