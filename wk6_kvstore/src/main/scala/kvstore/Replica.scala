6a91deda6337894f5963a90c485a2a5ce549f5f3
Norbert Scheller <nscheller@actian.com>
2013-12-17 15:50:34 +0100

    Step 1

--- wk6_kvstore/src/main/scala/kvstore/Replica.scala
+++ wk6_kvstore/src/main/scala/kvstore/Replica.scala
@@ -46,6 +46,8 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
   // the current set of replicators
   var replicators = Set.empty[ActorRef]
 
+  arbiter ! Join
+  
   def receive = {
     case JoinedPrimary   ⇒ context.become(leader)
     case JoinedSecondary ⇒ context.become(replica)
@@ -53,12 +55,19 @@ class Replica(val arbiter: ActorRef, persistenceProps: Props) extends Actor {
 
   /* TODO Behavior for  the leader role. */
   val leader: Receive = {
-    case _ =>
+    case Insert(key, value, id) =>
+      kv += (key -> value)
+      sender ! OperationAck(id)
+    case Remove(key, id) =>
+      kv -= key
+      sender ! OperationAck(id)
+    case Get(key, id) =>
+      sender ! GetResult(key, kv.get(key), id)
   }
 
   /* TODO Behavior for the replica role. */
   val replica: Receive = {
-    case _ =>
+    case Get(key, id) =>
+      sender ! GetResult(key, kv.get(key), id)
   }
-
 }
